3df77e88618265529a215704c38b921cbc5c929b
Esteban Beltran <sergies@gmail.com>
2021-07-14 16:34:56 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='kibbe',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=required,
+    entry_points={
+        'console_scripts': [
+            'kibbe = src.main:cli',
+        ],
+    },
+)
