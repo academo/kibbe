6e61dd8088bb3c56ca4586e401845e27ab78f3d1
Esteban Beltran <sergies@gmail.com>
2021-07-14 16:38:25 +0200

    Add license

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
+    download_url='https://github.com/academo/kibbe/releases',
     install_requires=required,
     entry_points={
         'console_scripts': [
