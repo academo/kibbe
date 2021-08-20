f10ff0d068f56ca5e8da83a9dcc2ee2bfd1b37f1
Esteban Beltran <sergies@gmail.com>
2021-08-20 13:41:29 +0200

    Add auto switch for existing contexts

--- src/commands/ctx.py
+++ src/commands/ctx.py
@@ -48,9 +48,25 @@ from src.util import get_valid_filename
     default=True,
 )
 def ctx(name, parent_path, source, branch, interactive, overwrite_branch, cd):
+    """
+    ctx is a wrapper kibbe subcommand for git worktree with some quality of life improvements.
+
+    It allows you to quickly switch and create git worktrees without having to type or memorize
+    all the git worktree parameterse.
+
+    ctx works better when you use it with tmux. In mac, if you use iterm2,
+    you can start tmux with `tmux -CC`. Install it first with `brew install tmux`
+
+    ctx is not intended to be a replacement for git worktree, if you can't perform the operation
+    you want with ctx please see the git worktree manual entry https://git-scm.com/docs/git-worktree
+    """
 
     # TODO check if the worktree exists and switch to it
     path_name = get_valid_filename(name)
+    existing_worktree = find_existing_worktree(path_name)
+
+    if existing_worktree:
+        return handle_existing_worktree(existing_worktree)
 
     if not branch:
         if interactive:
@@ -121,3 +137,59 @@ def ctx(name, parent_path, source, branch, interactive, overwrite_branch, cd):
         click.echo(
             "Changing to a worktree is only supported if you are running inside tmux"
         )
+
+
+def handle_existing_worktree(existing_worktree):
+    existing_path_name = existing_worktree["worktree"]
+    click.echo(
+        "Existing worktree with the same name found at "
+        + colored(existing_path_name, "yellow")
+    )
+    click.echo("Worktree branch: " + colored(existing_worktree["branch"], "blue"))
+    click.echo("Head commit: " + colored(existing_worktree["HEAD"], "blue"))
+    click.echo()
+
+    if Path(existing_path_name) == Path(os.getcwd()):
+        click.echo(colored("You are already on this worktree", "yellow"))
+        exit(0)
+
+    if not is_inside_tmux():
+        click.echo("You can switch to it by running: ")
+        click.echo(colored("cd %s" % existing_path_name, "blue"))
+        click.echo()
+        click.echo("Run this command inside tmux to automatically cd to it")
+    else:
+        click.echo("Tmux session detected. Changing to worktree")
+        current_pane = get_current_panel()
+        current_pane.send_keys("cd %s && nvm use" % existing_path_name)
+        exit(0)
+
+
+def find_existing_worktree(path_name):
+    worktrees = get_worktree_list()
+    existing_worktree = {}
+    for tree in worktrees:
+        path = Path(tree["worktree"])
+        if path.name == path_name:
+            existing_worktree = tree
+            break
+
+    return existing_worktree
+
+
+def get_worktree_list():
+    raw_list = subprocess.getoutput("git worktree list --porcelain")
+    worktrees = []
+    raw_list = raw_list.split("\n")
+
+    current = {}
+    for item in raw_list:
+        if not item:
+            worktrees.append(current)
+            current = {}
+            continue
+
+        [name, value] = item.split(" ")
+        current[name] = value
+
+    return worktrees
